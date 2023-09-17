9cca894241cde1a2c0088e1fdafbe4abd7ec544c
moviefan322 <philman202@yahoo.com>
2023-09-17 17:19:49 -0400

    adds edit button to player section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import { AiFillEdit } from "react-icons/ai";
+
 export default function Home() {
+  const [score, setScore] = useState(0);
+  const [score2, setScore2] = useState(0);
+  const [scoreRack, setScoreRack] = useState(0);
+  const [scoreRack2, setScoreRack2] = useState(0);
+  const [name, setName] = useState("Player 1");
+  const [name2, setName2] = useState("Player 2");
+
   return (
     <>
       <div className="scorebox">
         <div className="playerbox">
           <div className="name">
-            <p> Player 1</p>
+            <p>{name} </p>
+            <p className="edit">
+              <AiFillEdit />
+            </p>
           </div>
           <div className="scoreTotal">10</div>
           <div className="bottom">
@@ -23,14 +36,13 @@ export default function Home() {
           </div>
           <div className="scoreTotal">10</div>
           <div className="bottom">
-          <div className="scoreRack">
+            <div className="scoreRack">
               <p>6</p>
             </div>
             <div className="buttons2">
               <button className="plus">+</button>
               <button className="minus">-</button>
             </div>
-
           </div>
         </div>
       </div>
