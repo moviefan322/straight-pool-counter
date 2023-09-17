4459104e053b235b2178a1d4d05fa12420b40887
moviefan322 <philman202@yahoo.com>
2023-09-17 17:29:51 -0400

    adds remaining in rack

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,6 +46,9 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className="rackrem">
+        <p>Remaining in Rack: 0</p>
+      </div>
     </>
   );
 }
