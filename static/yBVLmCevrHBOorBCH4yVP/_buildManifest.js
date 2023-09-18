d757023a2ec54910c70f2d4e3f394fbb67a23413
moviefan322 <philman202@yahoo.com>
2023-09-18 00:18:32 -0400

    Updates

---
+++ static/yBVLmCevrHBOorBCH4yVP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-e9003506f41781d8.js","static/chunks/pages/index-986573a4648b744e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
