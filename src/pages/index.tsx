2feed274fb1b49fa857ca96e0f7a599bb18c345a
moviefan322 <philman202@yahoo.com>
2023-09-18 15:12:50 -0400

    testing PROPERLY configured

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiFillEdit, AiOutlineCheck } from "react-icons/ai";
 
 export default function Home() {
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,17 +1,17 @@
 /**
  * @jest-environment jsdom
  */
+import React from "react";
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Home from "@/pages/index";
+import Home from "../src/pages/index";
 
 describe("Home", () => {
   it("renders a heading", () => {
     render(<Home />);
 
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
-    });
+    const title = screen.getByText("14.1 Scorecard");
 
-    expect(heading).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
   });
 });
